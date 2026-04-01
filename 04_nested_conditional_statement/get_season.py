9db5e0170d7871a5bba23a306ea82c62b37b5867
Maloth Ganesh <malothganesh9676@gmail.com>
2026-04-01 22:12:08 +0530

    Add get_season.py to determine the season

---
+++ 04_nested_conditional_statement/get_season.py
@@ -0,0 +1,12 @@
+m = int(input())
+
+if m == 11 or m == 12 or m == 1:
+    print("Winter")
+elif m == 2 or m == 3 :
+    print("Spring")
+elif m == 4 or m == 5 or m == 6:
+    print("Summer")
+elif m == 7 or m == 8 :
+    print("Rainy")
+else:
+    print("Autumn")
