9b0463487792cc79f144d50a4f1a8a102387fc41
Maloth Ganesh <malothganesh9676@gmail.com>
2026-04-02 22:19:32 +0530

    Implement simple calculator with basic operations

---
+++ 04_nested_conditional_statement/simple_calculator.py
@@ -0,0 +1,14 @@
+O = input()
+A = int(input())
+B = int(input())
+
+if O == "+":
+    print(A + B)
+elif O == "-":
+    print(A - B)
+elif O == "*":
+    print(A * B)
+elif O == "/":
+    print(A / B)
+elif O == "%":
+    print(A % B)
