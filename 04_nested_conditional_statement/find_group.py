e9cb7664d6a0a24f0fd33299b3b1d844ff1d4505
Maloth Ganesh <malothganesh9676@gmail.com>
2026-04-03 21:41:08 +0530

    Add function to determine group based on input N

---
+++ 04_nested_conditional_statement/find_group.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+if N % 6 == 1:
+    print("Group 1")
+elif N % 6 == 2 :
+    print("Group 2")
+elif N % 6 == 3:
+    print("Group 3")
+elif N % 6 == 4:
+    print("Group 4")
+elif N % 6 == 5:
+    print("Group 5")
+elif N % 6 == 0:
+    print("Group 6")
