587ca1ceb0bad1e2fac16de9d873fd9ae94044d4
Maloth Ganesh <malothganesh9676@gmail.com>
2026-04-09 21:09:35 +0530

    Fix input comment in two_rightangle_triangle.py

---
+++ 05_loops_python/two_rightangle_triangle.py
@@ -0,0 +1,12 @@
+N = int(input()) # 3
+
+count = 1 
+while count <= N:
+    numbers = str(count) * count
+    print(numbers)
+    count = count + 1 
+count = 1
+while count <= N:
+    numbers = str(count) * count
+    print(numbers)
+    count = count + 1     
