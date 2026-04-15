91f15f285a2f94b56e7903c7d84f4a69f574c9c3
Maloth Ganesh <malothganesh9676@gmail.com>
2026-04-15 20:54:42 +0530

    Add square root comparison functionality

---
+++ 03_conditional_statements/square_root_of_a_number.py
@@ -0,0 +1,10 @@
+a = int(input()) # 64
+b = int(input()) # 8
+
+square_root_a = a ** 0.5 
+
+is_equal = square_root_a == b 
+if is_equal:
+    print("Square root of A is equal to B")
+else:
+    print("Square root of A is not equal to B")
