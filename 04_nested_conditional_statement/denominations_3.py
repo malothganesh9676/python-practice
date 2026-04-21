17c1aa8f9073cf64c8021610246951edcb5fe497
Maloth Ganesh <malothganesh9676@gmail.com>
2026-04-21 21:39:14 +0530

    Add comment to input line in denominations_3.py

---
+++ 04_nested_conditional_statement/denominations_3.py
@@ -0,0 +1,15 @@
+amount = int(input()) # 1543
+
+no_of_500s = amount // 500 
+remaining = amount % 500 
+
+no_of_50s = remaining // 50 
+remaining = remaining % 50 
+
+no_of_10s = remaining // 10 
+remaining = remaining % 10 
+
+no_of_1s = remaining
+
+result = ("500: " + str(no_of_500s)) + " " + ("50: " + str(no_of_50s)) + " " + ("10: " + str(no_of_10s)) + " " + ("1: " + str(no_of_1s))
+print(result)
